02600ef992a01e4c96bd19a3b6e8ca7e7504f4e4
vuquangminhpe <minhvqhe176726@fpt.edu.vn>
2025-03-18 00:27:42 +0700

    update api in client

--- client/src/apis/ai-gen.api.ts
+++ client/src/apis/ai-gen.api.ts
@@ -1,7 +1,9 @@
+import { ResponseAI } from '@/types/Ai.types'
+import { SuccessResponse } from '@/types/Utils.type'
 import http from '@/utils/http'
 
 const apiAIGEN = {
-  chatBotAiGen: (message: string) => http.post('/generate/chat', message),
-  getConversationAI: () => http.get('/conversation/chat')
+  chatBotAiGen: (message: string) => http.post<SuccessResponse<{ message: string }>>('/generate/chat', message),
+  getConversationAI: () => http.get<SuccessResponse<ResponseAI>>('/conversation/chat')
 }
 export default apiAIGEN
---
+++ client/src/types/Ai.types.ts
@@ -0,0 +1,8 @@
+export interface ResponseAI {
+  _id: string
+  sender_id: string
+  receive_id: string
+  content: string
+  createdAt: string
+  sender_info_username: string
+}
